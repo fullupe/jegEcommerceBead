d6a6ac0ba2a3a94059056f5f00fd953af1995f31
william <william@williams-MacBook-Pro.local>
2022-08-17 14:17:42 +0100

    fixed typo

--- components/FooterBanner.jsx
+++ components/FooterBanner.jsx
@@ -6,14 +6,7 @@ function FooterBanner({footerBanner}) {
   return (
     <div className="py-20 px-12 bg-[#f02d34] text-lg rounded-2xl relative h-96 md:h-[80%] flex leading-3 w-full text-white mt-32 ">
      <div className=" flex flex-col md:flex-row ">
-       {/* <div className="flex flex-col ">
-         <h3 className=" font-extrabold text-7xl ml-6 "></h3>
-         <p className="m-4">{footerBanner.discount}</p>
-         <h3 className="font-extrabold text-7xl ml-6">{footerBanner.largeText1}</h3>
-         <h3 className="font-extrabold text-7xl ml-6">{footerBanner.largeText2}</h3>
-
-         <p className="m-4">{footerBanner.saleTime}</p>
-       </div> */}
+      
        <div className="flex flex-col">
        <p className="m-">{footerBanner.smallText}</p>
        <h3 className="font-extrabold text-7xl ml-6 capitalize ">{footerBanner.midText}</h3>
